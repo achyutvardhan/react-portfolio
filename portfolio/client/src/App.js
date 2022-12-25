b1c0d6487814ba09148545166ee2d7616bcedc12
achyut vardhan <achyutvardhan234@gmail.com>
2022-12-25 16:20:44 +0530

    .

--- portfolio/client/src/App.js
+++ portfolio/client/src/App.js
@@ -1,15 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Profile from './PortfolioContainer/Home/Profile';
 import Footer from './PortfolioContainer/Footer/footer.jsx'
+import Layout from "./PortfolioContainer/Layout";
+import Error from "./PortfolioContainer/Error";
 import './App.css';
 
 function App() {
   return (
     <>
     
-    <div >
+    {/* <div >
       <Profile/>
     </div>
-      <Footer/>
+      <Footer/> */}
+      <BrowserRouter>
+      <Layout>
+        {/* <Nav /> */}
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Layout>
+    </BrowserRouter>
     </>
 
   );
