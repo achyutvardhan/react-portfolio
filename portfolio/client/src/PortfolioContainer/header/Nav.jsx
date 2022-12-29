a2dc82795f78583be403333fbbc19ba2eafafb31
achyut vardhan <achyutvardhan234@gmail.com>
2022-12-29 17:42:12 +0530

    Update Nav.jsx

--- portfolio/client/src/PortfolioContainer/header/Nav.jsx
+++ portfolio/client/src/PortfolioContainer/header/Nav.jsx
@@ -7,10 +7,10 @@ export default function Nav() {
       <div className="name">ACHYUT</div>
       <div className="route">
         <a href="/" className="navbtn">Home</a>
-        <a href="#about-me" className="navbtn">AboutMe</a>
+        <a href="/#about-me" className="navbtn">AboutMe</a>
         <a href="/Resume" className="navbtn">Resume</a>
         <a href="#" className="navbtn">Testimonial</a>
-        <a href="#" className="navbtn">ContactMe</a>
+        <a href="/#ContactMe" className="navbtn">ContactMe</a>
       </div>
       <Hamburger/>
     </header>
