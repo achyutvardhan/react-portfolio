c1de3bdae87317069f067af70a98c601a43d123d
achyut vardhan <achyutvardhan234@gmail.com>
2022-12-31 10:36:54 +0530

    testimonial complete

--- portfolio/client/src/PortfolioContainer/Testimonial/jsx/Carousel.jsx
+++ portfolio/client/src/PortfolioContainer/Testimonial/jsx/Carousel.jsx
@@ -2,20 +2,63 @@ import React from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import '../css/Carousel.css'
-export default function ContentBox() {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
+import db from "../db.json";
+import ContentBox from './ContentBox';
+export default function Carousel() {
+  const {data} = db
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplaySpeed: 0,
+    speed: 30000,
+    slidesToShow: 4.5,
+    slidesToScroll: 6,
+    autoplay : true,
+    accessibility:false,
+    draggable:false,
+    pauseOnHover:false,
+    centerPadding: '20px',
+    ease:'ease-out',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint:750,
+        settings:{
+          slidesToShow:2
+        }
+      },
+      {
+        breakpoint:520,
+        settings:{
+          slidesToShow:1.5
+        }
+      },
+      {
+        breakpoint:390,
+        settings:{
+          slidesToShow:1
+        }
+      }
+    ]
+  }
   return (
     <Slider {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
+      {data.map((Data,i)=>
+        <ContentBox Data = {Data} key = {i} />
+      )
+
+      }
     </Slider>
   )
 }
--- portfolio/client/src/PortfolioContainer/Testimonial/jsx/ContentBox.jsx
+++ portfolio/client/src/PortfolioContainer/Testimonial/jsx/ContentBox.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import '../css/ContentBox.css'
 export default function ContentBox(props) {
+    console.log(props);
   return (
     <>
     <div className="Testi-extrenal-container">
         <div className="Testi-internal-container">
             <div className="testi-para">
-                <p>{props.message}</p>
+                <p>{props.Data.message}</p>
                 {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aliquam esse blanditiis, dolorum harum assumenda ipsum neque placeat ratione. Neque sunt saepe ratione rerum, tempore doloribus quae officia odio odit!</p> */}
             </div>
             <div className="testi-img">
-                <img src={props.img} alt="img" />
+                <img src={props.Data.img} alt="img" />
                 {/* <img src={require('../../../assets/Home/achyut.jpg')} alt="img" /> */}
                 <div className="testi-name">
                    <div className="position" >
                     {/* <p>Achyut vardhan</p> */}
-                    <p>{props.name}</p>
+                    <p>{props.Data.name}</p>
                     </div> 
                     <div className="position" id='light'>
                         {/* <p>Founder</p> */}
-                        <p>{props.position}</p>
+                        <p>{props.Data.position}</p>
                     </div>
                 </div>
             </div>
