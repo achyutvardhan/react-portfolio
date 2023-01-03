05e9207d71ce35f080bec669745837ca4f614a14
achyut vardhan <achyutvardhan234@gmail.com>
2023-01-03 22:00:50 +0530

    .

--- portfolio/client/src/PortfolioContainer/AboutMe/details/AboutMeImg.jsx
+++ portfolio/client/src/PortfolioContainer/AboutMe/details/AboutMeImg.jsx
@@ -4,7 +4,7 @@ export default function AboutMeImg() {
   return (
     <div className="img-container">
       <div className="profile-img">
-        <img src={require('../../../assets/Home/achyut.jpg')} alt="" width={400} height={500} />
+        <img src={"https://i.ibb.co/kgjH97T/Whats-App-Image-2023-01-03-at-21-54-42.jpg"} alt="" width={400} height={500} />
       </div>
     </div>
   )
