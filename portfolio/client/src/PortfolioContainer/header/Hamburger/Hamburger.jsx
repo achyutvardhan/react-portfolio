3febebfc8dfb5b210fa4edfffdfc944d9ab87ccc
achyut vardhan <achyutvardhan234@gmail.com>
2023-01-03 19:33:06 +0530

    nav-bar complete

--- portfolio/client/src/PortfolioContainer/header/Hamburger/Hamburger.jsx
+++ portfolio/client/src/PortfolioContainer/header/Hamburger/Hamburger.jsx
@@ -1,21 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import'./Hamburger.css'
 export default function Hamburger() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [icon,setIcon] = useState(false)
+  const handleToggle = ()=>{
+    console.log("called")
+    setNavbarOpen(!navbarOpen);
+    setIcon(!icon)
+  }
+  const closeNav = ()=>{
+    setNavbarOpen(false)
+    setIcon(false)
+    if (icon) {
+      document.querySelector(".bar").style.display = "block"
+      document.querySelector(".cross").style.display = "none"
+      document.body.style.overflow = "scroll"
+    }
+    else{
+      document.querySelector(".bar").style.display = "none"
+      document.querySelector(".cross").style.display = "block"
+      document.body.style.overflow = "hidden"
+
+    }
+  }
+  const changeIcon = ()=>{ 
+    if (icon) {
+      document.querySelector(".bar").style.display = "block"
+      document.querySelector(".cross").style.display = "none"
+      document.body.style.overflow = "scroll"
+    }
+    else{
+      document.querySelector(".bar").style.display = "none"
+      document.querySelector(".cross").style.display = "block"
+      document.body.style.overflow = "hidden"
+
+    }
+  }
   return (
-    <div className="hamburger-container">
-        <div className="slide-bar">
+    <div className="hamburger-container" onClick={handleToggle} >
+        <div className="slide-bar" onClick={changeIcon} >
+          <div className="bar">
         <div className="ham-slide-bar"></div>
         <div className="ham-slide-bar"></div>
         <div className="ham-slide-bar"></div>
+          </div>
+          <div className="cross">
+            <div className="cross-bar">X</div>
+          </div>
         </div>
 
-        <div className="menu">
+        <div className={` ${navbarOpen ?"menu" : "hideMenu"}`}>
           <div className="route-ham">
-             <a href="/" className="navbtn-ham">Home</a>
-             <a href="#" className="navbtn-ham">AboutMe</a>
-             <a href="#" className="navbtn-ham">Resume</a>
-             <a href="#" className="navbtn-ham">ContactMe</a>
-             <a href="#" className="navbtn-ham">Testimonial</a>
+             <a href="/" className="navbtn-ham" onClick={closeNav}>Home</a>
+             <a href="/#about-me" className="navbtn-ham" onClick={closeNav}>AboutMe</a>
+             <a href="/Resume" className="navbtn-ham" onClick={closeNav}>Resume</a>
+             <a href="/#testimonial" className="navbtn-ham" onClick={closeNav}>Testimonial</a>
+             <a href="/#ContactMe" className="navbtn-ham" onClick={closeNav}>ContactMe</a>
           </div>
         </div>
       </div>
